254e13a2071d5c0442c6eeb5a96aab75862e6258
tpkn <tpkn@users.noreply.github.com>
2017-03-18 19:53:15 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# preference-manager
+# Preference Manager
+
 Save your app prefs into local config file
+
+
+## Save
+```javascript
+pm.save({x: 123, y: 456, width: 800, height: 600, godmod: true});
+```
+
+## Quick save
+This is 'lazy' method. Out of the box it saves four default params `{x, y, width, height}`. Link to current `BrowserWindow` is required!
+```javascript
+pm.save(BrowserWindow);
+```
+
+## Load
+```javascript
+var prefs = pm.load();
+if(prefs.hasOwnProperty('x') && prefs.hasOwnProperty('y')){
+    win.setPosition(prefs.x, prefs.y);
+}
+if(prefs.hasOwnProperty('width') && prefs.hasOwnProperty('height')){
+    win.setSize(prefs.width, prefs.height);
+}
+```
---
+++ preference-manager.js
@@ -0,0 +1,51 @@
+/**
+ * Preference Manager (v1.1.7.20170318), http://tpkn.me/
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+class PreferenceManager {
+   constructor(){
+      this.config_file = path.join(process.cwd(), "config.cfg");
+   }
+
+   /**
+    * Load prefs
+    * @return {Object}
+    */
+   load(){
+      let data = {};
+      if(fs.existsSync(this.config_file)){
+         data = JSON.parse(fs.readFileSync(this.config_file, 'utf8'));
+      }
+      return data;
+   }
+
+   /**
+    * Save prefs
+    * @param  {Object} data
+    */
+   save(data){
+      if(typeof data === 'undefined') throw new Error("No arguments were passed");
+      if(Object.keys(data).length === 0) throw new Error("Data is too short");
+
+      /**
+       * Template for quick save
+       */
+      if(data && data._events && data._events.closed){
+         data = {x: data.getPosition()[0], y: data.getPosition()[1], width: data.getSize()[0], height: data.getSize()[1]};
+      }
+
+      let save_data = data;
+      if(fs.existsSync(this.config_file)){
+         save_data = this.load();
+         for (let i in data){
+            save_data[i] = data[i];
+         }
+      }
+      fs.writeFileSync(this.config_file, JSON.stringify(save_data));
+   }
+}
+
+module.exports = new PreferenceManager;
